cf6b7c0a3671a5bb551efce8824fb244789df644
Daniel Du <73446163+72Rogan@users.noreply.github.com>
2022-12-02 00:44:41 +0000

    ss receber queries

--- G2/src/ss.py
+++ G2/src/ss.py
@@ -2,6 +2,8 @@ import socket
 from cache import Cache
 from parser_config import Parser_Config
 from logs import Logs
+from query import Query
+import threading
 import sys
 import time
 
@@ -25,7 +27,7 @@ class SS:
         self.logs.EV("log-file-create", self.ssConfig.dir_logLocal)
         self.ssCache = Cache()
         
-    def conecta(self):
+    def conecta_sp(self):
         ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         ss.connect((self.ip, self.porta))
         print(f"[SS] - SERVER CONNECTED")
@@ -72,8 +74,38 @@ class SS:
             ss.close()
             conecta = False
         self.logs.EV("end-of-connection")
+        
+    def conecta_cliente(self):
+        #print("[SERVER UDP MODE] - STARTING...")
+        serverUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        serverUDP.bind((self.ip, self.porta))
+            
+        print("[SERVER UDP MODE] - LISTENING...")
+        
+        while True:
+            (msg,add) = serverUDP.recvfrom(1024)
+            self.query.parse_message_condense(msg.decode('utf-8'))
+            self.logs.QR_QE(True, str(add), self.query.query_info_name + " " + self.query.query_info_type)
+            
+            clientMsg = "Message from Client:\n -> {}  ".format(msg.decode('utf-8'))
+            
+            bytesToSend = str.encode(self.query.origina_resposta(self.srvCache,self.query,clientMsg))
+            
+            clientIP  = "Client IP Address: {}".format(add)
+            
+            print(clientMsg)
+            print(clientIP)
+    
+            # Sending a reply to client
+            serverUDP.sendto(bytesToSend, add)
+            self.logs.RP_RR(True,str(add),  self.query.query_info_name + " " + self.query.query_info_type)
             
 if __name__ == "__main__":
+    
     ss = SS()
-    ss.conecta()
-    #print(ss.ssCache)
+    #print(srv.srvCache)
+    t1 = threading.Thread(target = srv.conecta_cliente)
+    t2 = threading.Thread(target = srv.conecta_sp)
+        
+    t1.start()
+    t2.run()
